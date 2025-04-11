606961901747516a08969f86abbc61327d06161d
cemtaskin82@gmail.com <cemtaskin82@gmail.com>
2025-04-11 11:16:11 +0300

    Search function added

--- src/components/student_list.jsx
+++ src/components/student_list.jsx
@@ -1,22 +1,72 @@
-import { Select, Space, Table, Tag } from "antd";
+import { Input, Select, Space, Table, Tag } from "antd";
+import Search from "antd/es/transfer/search";
+import { use, useEffect, useState } from "react";
 
 const StudentList = ({ students }) => {
+  const { Search } = Input;
+  const [filteredData, setFilteredData] = useState([...students]);
+  const [department, setDepartment] = useState("0");
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    search();
+  }, [searchText, department]);
   const distinctDepartments = [
     ...new Set(students.map((item) => item.department)),
   ];
+
+  const onDepartmentChange = (value) => {
+    setDepartment(value);
+  };
+
+  const onSearch = (value) => {
+    setSearchText(value);
+  };
+
+  const search = () => {
+    const searchValue = searchText.toLowerCase();
+    var data = [];
+    if (department === "0") {
+      data = students.filter(
+        (item) =>
+          item.number.toLowerCase().includes(searchValue) ||
+          item.name.toLowerCase().includes(searchValue) ||
+          item.surname.toLowerCase().includes(searchValue)
+      );
+    } else {
+      data = students.filter(
+        (item) =>
+          item.department === department &&
+          (item.number.toLowerCase().includes(searchValue) ||
+            item.name.toLowerCase().includes(searchValue) ||
+            item.surname.toLowerCase().includes(searchValue))
+      );
+    }
+
+    console.log(data);
+    setFilteredData(data);
+  };
 
   console.log(distinctDepartments);
   const getTitle = () => {
     return (
       <Space>
+        Filter
+        <Search
+          placeholder="Filter by number or name"
+          allowClear
+          style={{ width: 250 }}
+          enterButton="Search"
+          onSearch={onSearch}
+        ></Search>
         Department
-        <Select style={{ width: 150 }}>
+        <Select style={{ width: 150 }} onChange={onDepartmentChange}>
           <Select.Option value="0">All Departments</Select.Option>
           {distinctDepartments.map((item) => (
             <Select.Option value={item}>{item}</Select.Option>
           ))}
         </Select>
-        Student List : <Tag color="blue">{students.length}</Tag>
+        Student List : <Tag color="blue">{filteredData.length}</Tag>
       </Space>
     );
   };
@@ -29,7 +79,7 @@ const StudentList = ({ students }) => {
   ];
 
   return (
-    <Table title={getTitle} dataSource={students} columns={columns}></Table>
+    <Table title={getTitle} dataSource={filteredData} columns={columns}></Table>
   );
 };
 
