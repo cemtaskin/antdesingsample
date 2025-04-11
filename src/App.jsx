0f54cafaf36a8fa5e39d94655f80db2a04e3d4fb
cemtaskin82@gmail.com <cemtaskin82@gmail.com>
2025-04-11 12:20:59 +0300

    NoCodeApi Connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,30 @@
 import { LoginOutlined } from "@ant-design/icons";
 import { Button, DatePicker, Space, Watermark } from "antd";
 import StudentList from "./components/student_list";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  const students = [
-    { name: "Ali", surname: "Duru", number: "10120048458", department: "CE" },
-    { name: "Aliye", surname: "Duru", number: "10120048452", department: "CE" },
-    {
-      name: "Ahmet",
-      surname: "Durmaz",
-      number: "10120048455",
-      department: "CE",
-    },
-    {
-      name: "Fatma",
-      surname: "Çalışkan",
-      number: "10120048451",
-      department: "CE",
-    },
-    {
-      name: "ME ABC",
-      surname: "ME BC",
-      number: "10220048458",
-      department: "ME",
-    },
-    {
-      name: "ME XYZ",
-      surname: "ME XYZ",
-      number: "10220048452",
-      department: "ME",
-    },
-    {
-      name: "ME AAA",
-      surname: "ME AAA",
-      number: "10220048455",
-      department: "ME",
-    },
-    {
-      name: "ME BBB",
-      surname: "ME BBB",
-      number: "10220048451",
-      department: "ME",
-    },
-    {
-      name: "FE 001",
-      surname: "FE ABC",
-      number: "10222048451",
-      department: "FE",
-    },
-  ];
+  const [students, setStudents] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("url")
+      .then((res) => {
+        setStudents(res.data.data);
+        console.log(res.data.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error("Failed to fetch students:", err);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div style={{ width: "100%", height: "100%" }}>
-      <StudentList students={students} />
+      {!loading && <StudentList students={students} />}
     </div>
   );
 }
